1294fbc16ee63de4428c208624d04e23ad54a582
Asim <asim.awad98@gmail.com>
2025-05-11 00:16:03 +0200

    tested grading logic - working

--- scripts/aggregate_metrics.py
+++ scripts/aggregate_metrics.py
@@ -4,7 +4,8 @@ import os
 import numpy as np
 
 # List your grading report files here
-DISPLAY_NAME = "gpt-4-turbo_data_None_25_steps"
+DISPLAY_NAME = "RedHatAI_DeepSeek-R1-Distill-Qwen-14B-FP8-dynamic_data_None_25_steps"
+# "gpt-4o-mini_data_None_25_steps"
 # Metrics to aggregate: (Display Name, JSON Key)
 metrics = [
     ("Made Submission", "submission_exists"),
@@ -20,7 +21,11 @@ metrics = [
 results = {name: [] for name, _ in metrics}
 
 for file in os.listdir(f"{DISPLAY_NAME}/evaluation_results"):
+    if "_all_" in file:
+        print("skipping", file)
+        continue
     with open(f"{DISPLAY_NAME}/evaluation_results/{file}", "r") as f:
+        print("loading", file)
         data = json.load(f)
     reports = data["competition_reports"]
     total = len(reports)
@@ -36,7 +41,7 @@ for name, _ in metrics:
     mean = np.mean(vals)
     stderr = np.std(vals, ddof=1) / np.sqrt(len(vals))
     output.append(f"| {name} | {mean:.1f} ± {stderr:.1f} |")
-
+    print(f"| {name} | {mean:.1f} ± {stderr:.1f} |")
 # Save results to markdown file
 with open(f"{DISPLAY_NAME}/results_summary.md", "w") as f:
     f.write("\n".join(output))
